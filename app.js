107d6305055ce960310618580fb2a8735fd9ea7d
Trafi <theremandram@gmail.com>
2018-10-22 12:51:06 +0700

    Work on brain

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const {getHomePage, addPlayer} = require('./routes/index');
 
 var app = express();
 
@@ -17,34 +20,42 @@ var app = express();
 
 // view engine setup
 
+const db  = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+
+db.connect((err) => {
+  if (err) {
+      throw err;
+  }
+  console.log('Connected to database');
+});
+global.db = db;
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.bodyParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', getHomePage);
+//app.get('/add', addPlayerPage);
+//app.get('/edit/:id', editPlayerPage);
+//app.get('/delete/:id', deletePlayer);
+app.get('/add', addPlayer);
+//app.post('/edit/:id', editPlayer);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
+module.exports = {
+    getHomePage: (req, res) => {
+    res.render('index');
+    
+    },
+    addPlayer: (req, res) => {
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index1', { title: 'Express' });
+        let firstName = req.body.firstName;
+        let query = "INSERT INTO `json` (data) VALUES ('" + first_name +  "')";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    }
 
-});
+}
 
-var connection  = mysql.createConnection({
-	  host: "localhost",
-	  user: "root",
-	  password: "",
-	  database: "mydb"
-	});
 
-router.post('/users', function (req, res) {
-    connection.query('INSERT INTO json SET ?', req.body, 
-        function (err, result) {
-            if (err) throw err;
-            res.send('User added to database with ID: ' + result.insertId);
-        }
-    );
-});
-
-module.exports = router;
