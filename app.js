e3496d98f37c3158c1602e7e635eceada1282b13
Trafi <theremandram@gmail.com>
2018-10-23 14:26:59 +0700

    DB

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-
-//var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-const {getHomePage, addPlayer} = require('./routes/index');
-
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
+var usersRouter = require('./routes/users');
 
-// view engine setup
+const {getHomePage, addPlayer, addPlayerPage} = require('./routes/index');
+
+const port = 5000;
 
 const db  = mysql.createConnection({
   host: "localhost",
@@ -27,36 +17,68 @@ const db  = mysql.createConnection({
   database: "mydb"
 });
 
-db.connect((err) => {
+/*db.connect((err) => {
   if (err) {
       throw err;
   }
   console.log('Connected to database');
-});
-global.db = db;
+});*/
+
+const db_config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+    console.log('\n Connected to database \n');
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 
-app.set('views', path.join(__dirname, 'views'));
+// configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
 app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 
 app.get('/', getHomePage);
-//app.get('/add', addPlayerPage);
+app.get('/add', addPlayerPage);
 //app.get('/edit/:id', editPlayerPage);
 //app.get('/delete/:id', deletePlayer);
-app.get('/add', addPlayer);
+app.post('/add', addPlayer);
 //app.post('/edit/:id', editPlayer);
 
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
 
-
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,24 @@ module.exports = {
     },
     addPlayer: (req, res) => {
 
-        let firstName = req.body.firstName;
-        let query = "INSERT INTO `json` (data) VALUES ('" + first_name +  "')";
-        db.query(query, (err, result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            res.redirect('/');
-        });
+        //let firstName = req.body.firstName;
+        console.log("post");
+        //let query = "INSERT INTO `json` (data) VALUES ('" + firstName +  "')";
+        //db.query(query, (err, result) => {
+        //    if (err) {
+        //        return res.status(500).send(err);
+        //    }
+        //    res.redirect('/');
+        // });
+
+        console.log(req.params);
+        console.log('Request Type:', req.method);
+    },
+    addPlayerPage: (req, res) => {
+
+        console.log("get", res.query);
+        console.log(req.params);
+        console.log('Request Type:', req.method);
     }
 
 }
