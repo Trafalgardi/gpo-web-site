3911d4e03f4eabc2b5d77a2400978627bf744a82
Trafalgardi <theremandram@gmail.com>
2019-02-14 15:31:22 +0700

    Small chenged

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,57 +1,14 @@
-let qJSON = "";
+
+
+
+
+
 function printTest(params, id) {
     //let js = JSON.stringify(result[0].data);
     console.log(params)
     let temp = params.replace(/&quot;/g, '"');
 
     let json = JSON.parse(temp.substring(1, temp.length - 1))
-    //let json = JSON.parse(params);
-    //let json = {
-    //    "name": "НазваниеТеста",
-    //    "time": .1,
-    //    "html": "Коментарий есть",
-    //    "answers": [
-    //        "да",
-    //        "нет"
-    //    ],
-    //    "answerDefault": 0,
-    //    "questions": [
-    //        {
-    //            "question": "Впорос 1",
-    //            "answer": [
-    //                "Ответ1",
-    //                "Ответ2",
-    //                "Ответ3"
-    //            ]
-    //        },
-    //        {
-    //            "question": "Впорос 2",
-    //            "answer": [
-    //                "Ответ1",
-    //                "Ответ2",
-    //                "Ответ3"
-    //            ]
-    //        },
-    //        {
-    //            "question": "Впорос 3",
-    //            "answer": [
-    //                "Ответ1",
-    //                "Ответ2",
-    //                "Ответ3"
-    //            ]
-    //        },
-    //        {
-    //            "question": "Впорос 4",
-    //            "answer": [
-    //                "Ответ1",
-    //                "Ответ2",
-    //                "Ответ3"
-    //            ]
-    //        }
-    //    ],
-    //    "write": false,
-    //    "characters": "Abs"
-    //};
 
     console.log(json)
     if (json.time !== undefined && json.time > 0) {
@@ -64,20 +21,32 @@ function printTest(params, id) {
     } else {
         console.log('Коментария нет')
     }
+    //Определить ответ по умолчанию через условие (чтобы не выйти за рамки)
     let content = '';
-    
-    for (let i = 0; i < json.questions.length; i++) {
-        content += '<div class="form-group">';
-        content += '    <label>' + (i + 1) + '. ' + json.questions[i].question + '</label>';
-        if(json.answerDefault){
-        
-        }
-        let ans = json.answers === undefined ? json.questions[i].answer : json.answers;
-        let def = json.answerDefault !== undefined ? (json.answerDefault < 0 ? 0: json.answerDefault >= ans.length ? (ans.length - 1) : json.answerDefault): 0;
-        content += oneAnswer(ans, i, def);
-        content += '</div>';
-    }
+    switch (json.type) {
+        case 0: //Разные ответы на все тесты
+        content += allForAll(json.data, json.answerDefault)
+            break;
 
+        case 1: 
+        content += oneForAll(json.questions, json.answers, json.answerDefault) // 0 - answerDefault или dif или значение по умолчанию.
+            break;
+        
+        case 2:
+        content += justAnswers(json.data, json.answerDefault)
+            break;
+        
+        case 3:
+        content += textAnswers(json.questions, json.mask, json.pattern)
+            break;
+
+        case 4: 
+        content += htmlAnswers()
+            break;
+        default:
+            break;
+    }
+    
     content += '<div class="box-footer">';
     content += '    <input type="submit" class="btn btn-primary" value="Отправить ответы"></input>';
     content += '</div>';
@@ -87,53 +56,10 @@ function printTest(params, id) {
     content += '</div>';
 
     document.getElementById('questions').innerHTML = content;
+
+    //if(json.type == 3) $('.mask').mask(json.mask, {'translation': {0: {pattern: /d/}}});
     document.getElementById('testName').innerHTML = '<h1>' + json.name + '</h1>';
 
-    /* 
-    let temp = params.replace(/&quot;/g, '"');
-
-    let json = JSON.parse(temp.substring(1, temp.length - 1))
-
-    console.log(json); // this will show the info it in firebug console
-    
-    qJSON = json;
-    let content = '';
-    for (let i = 0; i < qJSON.questions.length; i++) {
-        content += '<div class="form-group">';
-        content += '    <label>' + (i + 1) + '. ' + qJSON.questions[i].question + '</label>';
-        for (let x = 0; x < qJSON.questions[i].answers.length; x++) {
-            content += '    <div class="radio">'
-            content += '        <label>'
-            content += '            <input type="radio" name="optionsRadios_' + i + '" value="' + qJSON.questions[i].answers[x] + '" >' + qJSON.questions[i].answers[x] // добавить защиту от дурака
-            content += '        </label>'
-            content += '    </div>'
-        }
-        content += '</div>';
-    };
-    //content += '<input type="text" id="answerInput" name="quantity" value="'+qJSON.questions.length+'">';
-    content += '<div class="box-footer">';
-    content += '    <input type="submit" class="btn btn-primary" value="Отправить ответы"></input>';
-    content += '</div>';
-
-    content += '<div class="box-footer">';
-    content += '    <input style="display:none" name="id" type="text" value="' + id + '"></input>';
-    content += '</div>';
-
-    content += '<div class="box-footer">';
-    content += '    <input style="display:none" name="size" type="text" value="' + qJSON.questions.length + '"></input>';
-    content += '</div>';
-
-    document.getElementById('timer').innerHTML = timer;
-    document.getElementById('testName').innerHTML = '<h1>' + qJSON.testName + '</h1>';
-    document.getElementById('questions').innerHTML = content;
-    let form = document.getElementById('questions');
-    $('.timer').startTimer({
-        onComplete: function () {
-            form.submit();
-        }
-    })
-
-*/
 }
 window.addEventListener("load", function () {
     //let button = document.querySelector("button");
@@ -161,14 +87,81 @@ function creatTimer(time) {
     })
 
 }
-function oneAnswer(parms, i, def) {
+
+
+function oneForAll(questions, ans, def){ //Когда одинаковые ответы для всех вопросов.
+    let content = '';
+    for (let i = 0; i < questions.length; i++) {
+        content += '<div class="form-group">';
+        content += '    <label>' + (i + 1) + '. ' + questions[i] + '</label>';
+        content += Answers(ans, i, def);
+        content += '</div>';
+        
+    }
+    return content;
+}
+
+function allForAll(data, def){ //Когда разные ответы для всех вопросов.
+    let content = '';
+    for (let i = 0; i < data.length; i++) {
+        content += '<div class="form-group">';
+        content += '    <label>' + (i + 1) + '. ' + data[i].question + '</label>';
+        content += Answers(data[i].answers, i, def);
+        content += '</div>';
+        
+    }
+    return content;
+}
+
+function justAnswers(data, def) {
+    let content = '';
+    for (let i = 0; i < data.length; i++) {
+        content += '<div class="form-group">';
+        content += '    <label>' + (i + 1) + '.</label>';
+        content += Answers(data[i].answers, i, def);
+        content += '</div>';
+        
+    }
+    return content;
+}
+
+function textAnswers(questions, def) {
+    let content = '';
+    for (let i = 0; i < questions.length; i++) {
+        content += '<div class="form-group">';
+        content += '    <label>' + (i + 1) +'. ' + questions[i] + '.</label>';
+        //content += Answers(data[i].answers, i, def);
+        content += '    <div>'
+        content += '        <input class="mask" name="inputFild_' + i + '" type="text" />';
+        content += '    </div>'
+        content += '</div>';
+        
+    }
+
+    return content;
+}
+
+function htmlAnswers() {
+    let content = '';
+    
+    content += '<div class="form-group">';
+    content += '    <div>'
+    content += '        <input class="mask" name="inputFild" type="text" />';
+    content += '    </div>'
+    content += '</div>';
+        
+    return content;
+}
+
+function Answers(parms, i, def) { //Мы передаём сюда массив ответов для вопроса, i номер вопроса, def - выбор значения по умолчанию
     let content = '';
     console.log(def)
+    def = def < 0 || def >= parms.length ? 0  : def;
     for (let x = 0; x < parms.length; x++) { //json.questions[i]
-        let checked = x == def ? 'checked': '';
+        let checked = x == def ? 'checked' : '';
         content += '    <div class="radio">'
         content += '        <label>'
-        content += '            <input type="radio" name="optionsRadios_' + i + '" value="' + parms[x] +'" '+ checked +' >' + parms[x] // добавить защиту от дурака
+        content += '            <input type="radio" name="optionsRadios_' + i + '" value="' + parms[x] + '" ' + checked + ' >' + parms[x] // добавить защиту от дурака
         content += '        </label>'
         content += '    </div>'
     }
