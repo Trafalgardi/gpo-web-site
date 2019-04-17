f1fb4fc45de4d3110172aa7cddbc9e7106b0982b
Trafalgardi <theremandram@gmail.com>
2019-04-18 02:05:09 +0700

    fix

--- routes/test.js
+++ routes/test.js
@@ -49,6 +49,7 @@ module.exports = {
                 connection.query(sql, function (err, results, fields) {
                     
                     let bantTests = JSON.parse(results[0].banTests);
+                    console.log(JSON.stringify(bantTests))
                     bantTests.id.push(body.id);
                     //console.log(JSON.stringify(bantTests))
                     let sqlUPDATE = "UPDATE `users` SET `banTests`= '"+JSON.stringify(bantTests)+"' WHERE `users`.`id` ="+ authData.user.id;
