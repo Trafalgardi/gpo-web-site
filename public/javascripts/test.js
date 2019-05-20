c3add2c56aca911be4d59832b476d032920d22eb
Trafalgardi <theremandram@gmail.com>
2019-05-20 23:56:59 +0700

    добавил тесты 24, 25

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,15 +1,11 @@
-
-
-
-
-
+let globalData = 0;
 function printTest(params, id) {
     //let js = JSON.stringify(result[0].data);
     console.log(params)
     let temp = params.replace(/&quot;/g, '"');
 
     let json = JSON.parse(temp.substring(1, temp.length - 1))
-
+    globalData = json;
     console.log(json)
     if (json.time !== undefined && json.time > 0) {
         creatTimer(json.time)
@@ -26,6 +22,7 @@ function printTest(params, id) {
         console.log('Коментария нет')
     }
     //Определить ответ по умолчанию через условие (чтобы не выйти за рамки)
+    console.log(json)
     let content = '';
     switch (json.type) {
         case 0: //Разные ответы на все тесты
@@ -47,13 +44,25 @@ function printTest(params, id) {
         case 4: 
         content += htmlAnswers()
             break;
+        case 5: 
+        content += noPatternText(json.data)
+            break;
+        case 6: 
+        content += '<div id="noPattern"></div>'
+            break;
+        case 7: 
+        content += '<div class="row" id="ImagePatter"></div>'
+            break;
         default:
             break;
     }
     
-    content += '<div class="box-footer">';
-    content += '    <input type="submit" class="btn btn-primary" value="Отправить ответы"></input>';
-    content += '</div>';
+    if(json.type != 5 && json.type != 6 && json.type != 7){
+        content += '<div class="box-footer">';
+        content += '    <input type="submit" class="btn btn-primary" value="Отправить ответы"></input>';
+        content += '</div>';
+    }
+   
 
     content += '<div class="box-footer">';
     content += '    <input style="display:none" name="id" type="text" value="' + id + '"></input>';
@@ -63,18 +72,9 @@ function printTest(params, id) {
 
     //if(json.type == 3) $('.mask').mask(json.mask, {'translation': {0: {pattern: /d/}}});
     document.getElementById('testName').innerHTML = '<h1>' + json.name + '</h1>';
-
+    if(json.type == 6) noPatternNumber(0);
+    if(json.type == 7) Image(1, 1, 1);
 }
-window.addEventListener("load", function () {
-    //let button = document.querySelector("button");
-    //button.addEventListener("click", function() {
-    //    for (let index = 0; index < qJSON.questions.length; index++) {
-    //        console.log(window.getElementByName("optionsRadios_"+index).value)
-    //        
-    //    }
-    //    console.log("Кнопка нажата.");
-    //});
-});
 
 function creatTimer(time) {
 
@@ -172,5 +172,251 @@ function Answers(parms, i, def) { //Мы передаём сюда массив 
     return content;
 }
 
+function noPatternText(json) {
+    
+    let random = getRandomInt(0, 10);
 
+    let content = '';
+    content += '<div id="hiddenInput">';
+    content += '<input style="display: none;" type="text" name="random" value="'+ random +'"/>'//отправлем выбраную нами последовательность
+    content += '<input style="display: none;" type="text" name="timer" id="stopwatch"></input>';
 
+    content += '</div>';
+    let data = json[random];
+    content += '<table class="table table-bordered" style="text-align: center;width: 1080px;margin: 0 auto;">'
+    content += '    <thead id="tHead">'
+    
+    content += '       <tr>'
+    for (let index = 0; index < data.length; index++) {
+        content += '<th scope="col">'
+        content +=  data[index];
+        content += '</th>'
+    }
+    content += '   </thead>'
+    
+    content += '</table>'
+    
+    content += '<div style="text-align: center;" id="btn" class="box-footer">';
+    content += '    <input onclick="AnswerTypeFive()" type="button" class="btn btn-primary" value="Начать"></input>';
+    content += '</div>';
+        
+    return content;
+}
+
+function noPatternNumber(int) {
+    let content = '';
+    let random = getRandomInt(0, 10);
+    let data = globalData.data[int];
+    content += '<div id="table">'
+    
+    content += '<div id="hiddenInput">';
+    content += '<input style="display: none;" type="text" name="random" value="'+ random +'"/>'//отправлем выбраную нами последовательность
+    content += '<input style="display: none;" type="text" name="timer" id="stopwatch"></input>';
+    content += '<input style="display: none;" type="text" id="answer" name="answer"></input>';
+    content += '</div>';
+    
+    content += '<table id="table" class="table table-bordered" style="text-align: center;width: 1080px;margin: 0 auto;">'
+    content += '    <thead id="tHead">'
+    
+    content += '       <tr>'
+
+    for (let index = 0; index < 10; index++) {
+        content += '<th scope="col">'
+        content +=  data[random][index];
+        content += '</th>'
+    }
+    content += '   </thead>'
+    
+    content += '</table>'
+    
+    content += '<div style="text-align: center;" id="btn" class="box-footer">';
+    content += '    <input onclick="AnswerTypeSix('+int+')" type="button" class="btn btn-primary" value="Начать"></input>';
+    content += '</div>';
+    
+    
+    content += '</div>'
+
+    document.getElementById('noPattern').innerHTML = content;
+}
+
+function AnswerTypeSix(int) {
+    let content = "";
+
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="0" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="1" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="2" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="3" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="4" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="5" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="6" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="7" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="8" /></th>'
+    content += '<th scope="row"><input style="width: 50px;" type="text" name="9" /></th>'
+    
+    Example1()
+    document.getElementById("tHead").innerHTML = content;
+    if(int == 2){
+        document.getElementById("btn").innerHTML = '<input onclick="TakeInput('+int+')" type="button" class="btn btn-primary" value="Отправить ответы"></input>';
+    }else{
+        document.getElementById("btn").innerHTML = '<input onclick="TakeInput('+int+')" type="button" class="btn btn-primary" value="Начать следующий этап"></input>';
+    }
+}
+function noPatternNumber1() {
+    noPatternNumber(1)
+}
+let jsonData = 
+    [
+        {
+            random: 0,
+            timer: "",
+            answer: [],
+        },
+        {
+            random: 0,
+            timer: "",
+            answer: [],
+        },
+        {
+            random: 0,
+            timer: "",
+            answer: [],
+        }
+    ]
+
+function TakeInput(int) {
+    
+    let form = document.forms[0];
+    let elems = form.elements;
+    console.log(elems) 
+    jsonData[int].random = elems[0].value
+    jsonData[int].timer = elems[1].value
+    for (let index = 2; index < 12; index++) {
+        jsonData[int].answer.push(elems[index].value)
+    }
+    console.log(jsonData)
+    if(int != 2) {
+        console.log(int)
+        noPatternNumber(int + 1)
+    }else {
+        $('<input>').attr({
+            type: 'hidden',
+            name: 'ans',
+            value: JSON.stringify(jsonData)
+        }).appendTo('form');
+        form.submit()
+    }
+    
+    
+}
+
+function AnswerTypeFive() {
+    let content = "";
+
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="0" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="1" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="2" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="3" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="4" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="5" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="6" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="7" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="8" required/></th>'
+    content += '<th scope="row"><input style="width: 125px;" type="text" name="9" required/></th>'
+    
+    Example1()
+    document.getElementById("tHead").innerHTML = content;
+    document.getElementById("btn").innerHTML = '<input type="submit" class="btn btn-primary" value="Отправить ответы"></input>';
+    
+}
+
+function Image(page, stage, lvl) {
+    $(".row").empty()
+    
+    let imgCount = 0;
+    if(lvl == 1){
+        imgCount = 2
+    }else if(lvl == 2){
+        imgCount = 3
+    }else {
+        imgCount = 4
+    }
+    if(page==2){
+        for (let i = 1; i <= imgCount; i++) {
+            $('<img>').attr({
+                id: 'img',
+                src: "/img/lvl_"+lvl+"/page_"+page+"/"+i+".png",
+                style: "width: 700px; height: 430px; margin-left: auto; margin-right: auto;",
+                onClick: "nextStage("+stage+", "+lvl+", "+i+")"
+            }).appendTo('.row');    
+        }
+    }else {
+        for (let i = 1; i <= imgCount; i++) {
+            $('<img>').attr({
+                id: 'img',
+                src: "/img/lvl_"+lvl+"/page_"+page+"/"+i+".png",
+                style: "width: 700px; height: 430px; margin-left: auto; margin-right: auto;"
+            }).appendTo('.row');    
+        }
+    }
+    
+    $('<div>').attr({
+        id: 'divBtn',
+        style: 'text-align: center;'
+    }).appendTo('#questions');  
+    if(page == 1){
+        $('<input>').attr({
+            id: 'btn',
+            type: 'button', 
+            class: "btn btn-primary",
+            value: "Начать",
+            onClick: "nextPage("+lvl+")"
+        }).appendTo('#divBtn'); 
+    }else {
+        $("questions").empty()
+    }
+    
+ 
+}
+function nextPage(lvl) {
+    Image(2, 2, lvl)
+}
+function nextStage(stage, number) {
+    alert(stage, number)
+}
+function CreatQuestionImg(number) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+}
+
+function Example1() {
+    var $stopwatch, // Stopwatch element on the page
+        incrementTime = 70, // Timer speed in milliseconds
+        currentTime = 0, // Current time in hundredths of a second
+        updateTimer = function() {
+            $stopwatch.val(formatTime(currentTime));
+            currentTime += incrementTime / 10;
+        },
+        init = function() {
+            $stopwatch = $('#stopwatch');
+            Example1.Timer = $.timer(updateTimer, incrementTime, true);
+        };
+    $(init);
+};
+// Common functions
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {str = '0' + str;}
+    return str;
+}
+function formatTime(time) {
+    var min = parseInt(time / 6000),
+        sec = parseInt(time / 100) - (min * 60),
+        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
