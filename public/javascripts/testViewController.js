a8faa08b60a1c3cbc9b69c150281d7dbf73542d7
qofi26 <qofi26@gmail.com>
2020-05-24 11:39:53 +0700

    Рефакторинг создание html элементов

--- public/javascripts/testViewController.js
+++ public/javascripts/testViewController.js
@@ -1,4 +1,5 @@
-const quiz = document.getElementById("quiz")
+const quiz = "quiz";
+const quiz_btn_container = "quiz-btn-container";
 
 //#region const categories 
 const categories_timer = "categories_timer";
@@ -16,36 +17,56 @@ const question_content = "question_content";
 //#endregion
 
 //#region const buttons
-const quiz_btn_container = document.getElementById("quiz-btn-container")
 const btn_skip_remember_time = "btn_skip_remember_time"
 //#endregion
 
-quiz_btn_container.innerHTML = `
-<button type="button" id="previous">Предыдущий вопрос</button>
-<button type="button" id="next">Следующий вопрос</button>
-<button type="submit" id="submitBtn">Отправить ответы</button>
-<button type="button" style="display:none" id="start">Начать</button>`;
+// quiz_btn_container.innerHTML = `
+// <button type="button" id="previous">Предыдущий вопрос</button>
+// <button type="button" id="next">Следующий вопрос</button>
+// <button type="submit" id="submitBtn">Отправить ответы</button>
+// <button type="button" style="display:none" id="start">Начать</button>`;
+
+const current_data = {
+    data: {},
+    question_id: 0,
+    category_id: 0,
+    get current_category() {
+        return this.data.categories[this.category_id];
+    },
+    get current_question() {
+        return this.current_category.questions[question_id];
+    },
+    get test_name() {
+        return this.data.name;
+    },
+    get test_time() {
+        return this.data.time;
+    },
+    get test_tutorial() {
+        return this.data.tutorial;
+    }
+};
 
 function mainFunc(json, id) {
-    quiz_btn_container.innerHTML = "";
-    quiz.innerHTML = "";
+    setInnerHTML(quiz_btn_container, "")
+    setInnerHTML(quiz, "");
 
-    setName(json.name, "testName")
+    current_data.data = json;
+    current_data.question_id = 0;
+    current_data.category_id = 0;
+    console.log(current_data);
+
+    setInnerHTML("testName", current_data.test_name)
 
     //Таймер на тест
-    createTimer(json.time, "timer");
+    createTimer(current_data.test_time, "timer", submitTest);
 
     //Туторила
-    setTutorial(json.tutorial)
+    setTutorial(current_data.test_tutorial)
 
     //Создание категорий
-    createCategories(json.categories[0])
+    createCategory()
 
-}
-
-function setName(name, elementId = "testName") {
-    var element = document.getElementById(elementId);
-    element.innerHTML = name;
 }
 
 function setTutorial(tutorial) {
@@ -55,12 +76,40 @@ function setTutorial(tutorial) {
     }
     return true
 }
+//#region Создание категорий
+function createCategory() {
+    setInnerHTML(quiz, "");
+
+    let category = current_data.current_category;
+    let content = "";
+    content += `
+        <div id="${categories_timer}"></div>
+        <div id="${categories_name}">${category.name}</div>
+        <div id="${categories_content}"></div>
+    `;
+    insertHtml(quiz, content)
+    createTimer(category.time, categories_timer, () => categoriesTimer())
+    let isTutorial = setTutorial(category.tutorial)
+    if (isTutorial) {
+        return
+    }
+    createQuestion(category.questions, 0)
+
+}
+
+function categoriesTimer() {
+    console.log("Таймер категории")
+}
+//#endregion
+
 //#region Создание вопроса
 function createQuestion(questions, id = 0) {
+    setInnerHTML(categories_content, "")
+    setInnerHTML(quiz_btn_container, "")
+
     let currentQuestion = questions[id];
     let time_to_remember = currentQuestion.time_to_remember;
-    quiz.innerHTML = "";
-    quiz_btn_container.innerHTML = "";
+
     let img = ""
     currentQuestion.images.forEach(element => {
         img += `<img src="/images/${element}">`;
@@ -72,24 +121,28 @@ function createQuestion(questions, id = 0) {
     <div id="${question_img_container}">${img}</div>
     <div id="${question_description}">${currentQuestion.description}</div>
     <div id="${question_content}"></div>`;
-    insertHtml(quiz, content)
+    insertHtml(categories_content, content)
     if (currentQuestion.can_skip_remember_time && currentQuestion.time_to_remember > 0) {
-        quiz_btn_container.innerHTML = `<button type="button" id="${btn_skip_remember_time}">Продолжить</button>`
-        let btn = document.getElementById(btn_skip_remember_time).onclick = cancelTimerToRemember;
+        setInnerHTML(quiz_btn_container, `<button type="button" id="${btn_skip_remember_time}">Продолжить</button>`)
+        addListenerOnClick(btn_skip_remember_time, () => $(`#timer_${question_timer_to_remember}`).trigger('complete'))
     }
-    createTimer(time_to_remember, question_timer_to_remember, () => timeToRemember(questions, id), () => createAnswers(questions, id))
+    createTimer(time_to_remember, question_timer_to_remember, () => endTimeToRemember(questions, id), () => createAnswers(questions, id))
 }
 
-function cancelTimerToRemember() {
-    $(`#timer_${question_timer_to_remember}`).trigger('complete');
-    document.getElementById(question_timer_to_remember).innerHTML = ""
-    quiz_btn_container.removeChild(document.getElementById(btn_skip_remember_time))
+
+function endTimeToRemember(questions, id) {
+    console.log("endTimeToRemember")
+    setInnerHTML(question_img_container, "");
+    setInnerHTML(question_description, "");
+    setInnerHTML(question_timer_to_remember, "");
+    removeChild(quiz_btn_container, btn_skip_remember_time)
+    createAnswers(questions, id)
 }
 
 function createAnswers(questions, id) {
     let currentQuestion = questions[id];
     let answers = currentQuestion.answers
-    
+
     switch (answers.type) {
         case 0:
             answers_0(questions, id)
@@ -102,26 +155,49 @@ function createAnswers(questions, id) {
     }
 
 }
+
+function addDataForm(name, value) {
+    let element_name = document.getElementById(`form_answers_${name}`)
+
+    if (element_name) {
+        element_name.value = value
+        return
+    }
+    let content = `<input type="text" id="form_answers_${name}" name="form_answers_${name}" value="${value}">`
+    insertHtml("form", content)
+
+}
+
+function next(questions, id) {
+    let nextId = id + 1;
+    console.log(questions[nextId])
+    if (questions.length > nextId) {
+        createQuestion(questions, nextId)
+    } else {
+        alert("test off")
+    }
+}
 //#region Answers template
 function answers_2(questions, id) {
     let data = questions[id].answers.data
     let content = `<input type="text" id="form_answers_${id}" name="form_answers_${id}" value="" require>`
     quiz_btn_container.innerHTML = `<button type="button" id="btn_next">Продолжить</button>`
-    let btn = document.getElementById("btn_next").onclick = function () {
+    addListenerOnClick("btn_next", function () {
         var answer = document.getElementById(`form_answers_${id}`)
         let isCheked = false;
         console.log(answer);
-        
+
         isCheked = answer.value != ""
-        if(isCheked==false){
+        if (isCheked == false) {
             alert("Впишите ответ")
             return
         }
         addDataForm(id, answer.value)
         next(questions, id)
-    };
-    document.getElementById(question_content).innerHTML = content;
+    });
+    setInnerHTML(question_content, content)
 }
+
 function answers_0(questions, id) {
     let data = questions[id].answers.data
     let content = '';
@@ -134,84 +210,38 @@ function answers_0(questions, id) {
             </label>
         </div>`
     }
-    quiz_btn_container.innerHTML = `<button type="button" id="btn_next">Продолжить</button>`
-    let btn = document.getElementById("btn_next").onclick = function () {
+    setInnerHTML(quiz_btn_container, `<button type="button" id="btn_next">Продолжить</button>`)
+    addListenerOnClick("btn_next", function () {
         var answer = document.getElementsByName("answer")
         let isCheked = false;
         let currentElement = ""
         for (let i = 0; i < answer.length; i++) {
             const element = answer[i];
-            if(element.checked == true) {
+            if (element.checked == true) {
                 isCheked = true;
                 currentElement = element
                 break
             }
         }
-        if(isCheked==false){
+        if (isCheked == false) {
             alert("Выберите ответ")
             return
         }
         addDataForm(id, currentElement.value)
         next(questions, id)
 
-    };
-    document.getElementById(question_content).innerHTML = content;
-}
-function addDataForm(name, value) {
-    let element_name = document.getElementById(`form_answers_${name}`)
-    
-    if(element_name){
-        element_name.value = value
-        return
-    }
-    let content = `<input type="text" id="form_answers_${name}" name="form_answers_${name}" value="${value}">`
-    var form = document.getElementById("form")
-    insertHtml(form, content)
-    
-}
-function next(questions, id) {
-    let nextId = id+1;
-    console.log(questions[nextId])
-    if(questions.length > nextId){
-        createQuestion(questions, nextId)
-    }else{
-        alert("test off")
-    }
+    });
+    setInnerHTML(question_content, content)
 }
 //#endregion
 
-function createImg(container_id, path) {
-    let content = `<img src="${path}">`;
-    insertHtml(document.getElementById(container_id), content)
-}
 
-function timeToRemember(questions, id) {
-    document.getElementById(question_img_container).innerHTML = ""
-    document.getElementById(question_description).innerHTML = ""
-    createAnswers(questions, id)
-}
+
+
 //#endregion
-//#region Создание категорий
 
-function createCategories(categories) {
-    console.log(categories)
-    let content = "";
-    content += `
-    <div id="${categories_timer}"></div>
-    <div id="${categories_name}">${categories.name}</div>
-    <div id="${categories_content}"></div>`;
-    insertHtml(quiz, content)
-    createTimer(categories.time, categories_timer, () => categoriesTimer())
-    let isTutorial = setTutorial(categories.tutorial)
-    if (isTutorial) {
-        return
-    }
-    createQuestion(categories.questions, 0)
-
-}
-//#endregion
 //#region Timer
-function createTimer(time, elementId = "timer", callback = () => document.forms[0].submit(), callback_faild = () => console.log("Таймера нет")) {
+function createTimer(time, elementId, callback, callback_faild = () => console.log("Таймера нет")) {
     if (!(time !== undefined && time > 0)) {
         callback_faild()
         return
@@ -221,18 +251,25 @@ function createTimer(time, elementId = "timer", callback = () => document.forms[
     let timer = '';
     timer += `<h5 id="timer_${elementId}" class='timer' data-minutes-left='${min}'></h5>`;
     timer += "<section class='actions'></section>";
-    document.getElementById(elementId).innerHTML = timer;
+    setInnerHTML(elementId, timer)
 
     $(`#timer_${elementId}`).startTimer({
         onComplete: callback
     })
 }
 
-function categoriesTimer() {
-    console.log("Таймер категории")
+function submitTest() {
+    document.forms[0].submit()
+}
+//#endregion
+
+//#region HTML helper
+function setInnerHTML(elementId, value) {
+    var element = document.getElementById(elementId);
+    if (element)
+        element.innerHTML = value;
 }
 
-//#endregion
 /**
  * @position {string} beforebegin | afterbegin | beforeend | afterend
  *  <!-- beforebegin -->
@@ -243,6 +280,25 @@ function categoriesTimer() {
  *  \</p>
  *  <!-- afterend -->
  */
-function insertHtml(element, content, position = 'beforeend') {
-    element.insertAdjacentHTML(position, content)
+function insertHtml(elementId, content, position = 'beforeend') {
+    let element = document.getElementById(elementId)
+    if (element)
+        element.insertAdjacentHTML(position, content)
 }
+
+function removeChild(elementId, childId) {
+    let element = document.getElementById(elementId);
+    if (element) {
+        let child = document.getElementById(childId)
+        if (child)
+            element.removeChild(child)
+    }
+}
+
+function addListenerOnClick(elementId, callback) {
+    let element = document.getElementById(elementId);
+    if (element) {
+        element.onclick = callback;
+    }
+}
+//#endregion
